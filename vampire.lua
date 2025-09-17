be61c702422941532fd45629eb72d7e445b4bf9e
u5358667550-ui <u5358667550@gmail.com>
2025-09-17 23:37:01 +0100

    Create vampire.lua

---
+++ vampire.lua
@@ -0,0 +1,10 @@
+if game.PlaceId == none rn then
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "Vampire.lua", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+
+
+
+
+end
+OrioinLib.In
+it()
